cd4e3255ed2cf8805cbc369dea41cf3fb41e4ec2
heshima243 <heshimajulien.official@gmail.com>
2023-09-23 09:45:32 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// ZNlAh2mAZnrCF0Bd mongodb+srv://julien:ZNlAh2mAZnrCF0Bd@testtask.arbjlzi.mongodb.net/?retryWrites=true&w=majority
+
+const express = require('express')
+const {gql, ApolloServer} = require('apollo-server-express')
+
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
+
+const mongoose = require('mongoose')
+
+
+
+async function startServer(){
+const app = express()
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+await apolloServer.start()
+
+apolloServer.applyMiddleware({app:app})
+
+app.use((req,res)=>{
+    res.send("hello from apollo expresss")
+})
+
+await mongoose.connect('mongodb+srv://julien:ZNlAh2mAZnrCF0Bd@testtask.arbjlzi.mongodb.net/?retryWrites=true&w=majority',{
+useUnifiedTopology: true,
+useNewUrlParser: true
+})
+console.log('mongoose connected');
+
+app.listen(8000, ()=> console.log('server running on 8000'))
+}
+startServer()
